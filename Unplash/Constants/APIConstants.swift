ca63f44bb074af844e205c2f1af8526f371c3ee3
Alex <nek2393@gmail.com>
2020-01-08 16:19:52 +0300

    Change api key

--- Unplash/Constants/APIConstants.swift
+++ Unplash/Constants/APIConstants.swift
@@ -1,5 +1,5 @@
 struct APIConstants {
-  static let accessKey = "6a5917b4c5eae9fd55fd558cff0a158dbe6b521de28c017800de6e402407046c"
+  static let accessKey = "37eaf849b490637cf74de2aba602ce53fd9bae7a576f31b61962a18bc73114d6"
   
   static let unplash = "https://api.unsplash.com"
   static func collection(id: Int) -> String {
